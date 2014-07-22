d98c08929bc9115838ca15e8865eb91a0d749ab9
Scott Hatcher <scott@scotthatcher.com>
2014-07-22 08:59:48 -0700

    Updates

---
+++ js/docs-setup.js
@@ -0,0 +1,34 @@
+NG_DOCS={
+  "sections": {
+    "api": "Sync Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "angularPoint.syncService",
+      "shortName": "angularPoint.syncService",
+      "type": "service",
+      "moduleName": "angularPoint",
+      "shortDescription": "Supports 3-way data binding if you decide to incorporate firebase (any change by any user",
+      "keywords": "active alerting allow angularfire angularpoint apconfig api apsyncservice automatically better binding change changes create current data decide determine event events execute firebase firebaseurl folder function generate ignore incorporate installed isn item list method mirrored model notified notify object occurred offline order point query register request responsible saved servervalue service set sharepoint single stored subscribers subscription supports sync synchronizedata syncpoint syncservice time timestamp title todo update updatequery url user users work xml"
+    },
+    {
+      "section": "api",
+      "id": "SyncPoint",
+      "shortName": "SyncPoint",
+      "type": "function",
+      "moduleName": "SyncPoint",
+      "shortDescription": "Constructor to handle notifying models when data is updated.",
+      "keywords": "allows angularpoint apconfig api callback change data events execute firebaseurl function handle listening model models notified notify notifying object query registerchange subscribers subscribetochanges sync syncpoint updated updatequery users"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "startPage": "/api/angularPoint.syncService",
+  "scripts": [
+    "angular.js",
+    "angular-animate.min.js"
+  ]
+};
