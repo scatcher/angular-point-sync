89e433721b3153c3e12e5687ef980072f6e56528
Scott Hatcher <github@scotthatcher.com>
2019-02-22 13:03:11 -0800

    Add stronger typing on list item change types.  Major release because it's now bound to angular-point >= 6.5.0

--- src/sync-point.factory.ts
+++ src/sync-point.factory.ts
@@ -1,133 +1,132 @@
-import { Model } from 'angular-point';
+import { ListItemChangeType, Model } from 'angular-point';
 import * as firebase from 'firebase';
 import * as _ from 'lodash';
-
 import { $firebaseArray, $rootScope, serviceIsInitialized } from './sync.service';
 
 export interface ISyncServiceChangeEvent {
-    changeType: 'add' | 'update' | 'delete';
-    listItemId: number;
-    userId: number;
-    time: number;
+  changeType: ListItemChangeType;
+  listItemId: number;
+  userId: number;
+  time: number;
 }
 
 export interface ISyncServiceInitializationParams {
-    userId: number;
-    firebaseRef: firebase.database.Reference;
+  userId: number;
+  firebaseRef: firebase.database.Reference;
 }
 
 export interface ISyncPoint {
-    eventLogLength: number;
-    recentEvents: ISyncServiceChangeEvent[];
-    registerChange(changeType: string, listItemId: number);
-    subscribeToChanges(callback: Function, unsubscribeOnStateChange: boolean): Function;
-    unsubscribe(callback);
+  eventLogLength: number;
+  recentEvents: ISyncServiceChangeEvent[];
+  registerChange(changeType: string, listItemId: number);
+  subscribeToChanges(callback: Function, unsubscribeOnStateChange: boolean): Function;
+  unsubscribe(callback);
 }
 
 export class SyncPoint implements ISyncPoint {
-    eventLogLength = 10;
-    changeNotifier: firebase.database.Reference;
-    recentEvents;
-    /** Container to hold all current subscriptions for the model */
-    subscriptions = [];
+  eventLogLength = 10;
+  changeNotifier: firebase.database.Reference;
+  recentEvents;
+  /** Container to hold all current subscriptions for the model */
+  subscriptions = [];
 
-    constructor(private model: Model) {
-        const syncPoint = this;
+  constructor(private model: Model) {
+    const syncPoint = this;
 
-        serviceIsInitialized.then((initializationParams: ISyncServiceInitializationParams) => {
-            syncPoint.changeNotifier = initializationParams.firebaseRef.child('changes/' + model.list.title);
+    serviceIsInitialized.then((initializationParams: ISyncServiceInitializationParams) => {
+      syncPoint.changeNotifier = initializationParams.firebaseRef.child('changes/' + model.list.title);
 
-            const query = syncPoint.changeNotifier.limitToLast(syncPoint.eventLogLength);
+      const query = syncPoint.changeNotifier.limitToLast(syncPoint.eventLogLength);
 
-            syncPoint.recentEvents = $firebaseArray(query);
+      syncPoint.recentEvents = $firebaseArray(query);
 
-            syncPoint.recentEvents.$loaded().then(eventArray => {
-                /** Fired when anyone updates a list item */
-                syncPoint.recentEvents.$watch(log => {
-                    if (log.event === 'child_added') {
-                        const newEvent: ISyncServiceChangeEvent = syncPoint.recentEvents.$getRecord(log.key);
-                        /** Capture if event was caused by current user */
-                        const externalTrigger = newEvent.userId !== initializationParams.userId;
-                        syncPoint.processChanges(newEvent, externalTrigger);
-                    }
-                });
-            });
+      syncPoint.recentEvents.$loaded().then(eventArray => {
+        /** Fired when anyone updates a list item */
+        syncPoint.recentEvents.$watch(log => {
+          if (log.event === 'child_added') {
+            const newEvent: ISyncServiceChangeEvent = syncPoint.recentEvents.$getRecord(log.key);
+            /** Capture if event was caused by current user */
+            const externalTrigger = newEvent.userId !== initializationParams.userId;
+            syncPoint.processChanges(newEvent, externalTrigger);
+          }
         });
+      });
+    });
+  }
+
+  /**
+   *
+   * @param {ISyncServiceChangeEvent} newEvent Details of event.
+   * @param {boolean} externalTrigger Was the changed caused by another user.
+   */
+  private processChanges(newEvent: ISyncServiceChangeEvent, externalTrigger: boolean): void {
+    const syncPoint = this;
+    /** Notify subscribers */
+    _.each(syncPoint.subscriptions, callback => {
+      if (_.isFunction(callback)) {
+        callback(newEvent, externalTrigger);
+      }
+    });
+  }
+
+  /**
+   * @ngdoc function
+   * @name SyncPoint.registerChange
+   * @methodOf SyncPoint
+   * @description
+   * Notify all other users listening to this model that a change has been made.
+   */
+  registerChange(changeType: string, listItemId: number) {
+    const syncPoint = this;
+    serviceIsInitialized.then(initializationParams => {
+      if (syncPoint.recentEvents.length >= syncPoint.eventLogLength) {
+        /** Trim the log to prevent unnecessary size */
+        syncPoint.recentEvents.$remove(0);
+      }
+
+      syncPoint.recentEvents.$add({
+        changeType: changeType,
+        listItemId: listItemId,
+        userId: initializationParams.userId,
+        time: firebase.database.ServerValue.TIMESTAMP,
+      });
+    });
+  }
+
+  /**
+   * @ngdoc function
+   * @name SyncPoint.subscribeToChanges
+   * @methodOf SyncPoint
+   * @description
+   * Allows subscribers (controllers & services) to be notified when change is made.
+   *
+   * @param {function} callback Callback to execute after a change is made.
+   * @param {boolean} [unsubscribeOnStateChange = true]
+   * @returns {function} Function used to unsubscribe.
+   */
+  subscribeToChanges(callback: Function, unsubscribeOnStateChange = true): Function {
+    const syncPoint = this;
+    if (syncPoint.subscriptions.indexOf(callback) === -1) {
+      /** Only register new subscriptions, ignore if subscription already exists */
+      syncPoint.subscriptions.push(callback);
     }
 
-    /**
-     *
-     * @param {ISyncServiceChangeEvent} newEvent Details of event.
-     * @param {boolean} externalTrigger Was the changed caused by another user.
-     */
-    private processChanges(newEvent: ISyncServiceChangeEvent, externalTrigger: boolean): void {
-        const syncPoint = this;
-        /** Notify subscribers */
-        _.each(syncPoint.subscriptions, callback => {
-            if (_.isFunction(callback)) {
-                callback(newEvent, externalTrigger);
-            }
-        });
+    const unsubscribe = () => this.unsubscribe(callback);
+
+    if (unsubscribeOnStateChange) {
+      /** Unsubscribe from notifications when we leave this state */
+      $rootScope.$on('$stateChangeStart', () => {
+        unsubscribe();
+      });
     }
 
-    /**
-     * @ngdoc function
-     * @name SyncPoint.registerChange
-     * @methodOf SyncPoint
-     * @description
-     * Notify all other users listening to this model that a change has been made.
-     */
-    registerChange(changeType: string, listItemId: number) {
-        const syncPoint = this;
-        serviceIsInitialized.then(initializationParams => {
-            if (syncPoint.recentEvents.length >= syncPoint.eventLogLength) {
-                /** Trim the log to prevent unnecessary size */
-                syncPoint.recentEvents.$remove(0);
-            }
+    return unsubscribe;
+  }
 
-            syncPoint.recentEvents.$add({
-                changeType: changeType,
-                listItemId: listItemId,
-                userId: initializationParams.userId,
-                time: firebase.database.ServerValue.TIMESTAMP,
-            });
-        });
+  unsubscribe(callback) {
+    const index = this.subscriptions.indexOf(callback);
+    if (index !== -1) {
+      this.subscriptions.splice(index, 1);
     }
-
-    /**
-     * @ngdoc function
-     * @name SyncPoint.subscribeToChanges
-     * @methodOf SyncPoint
-     * @description
-     * Allows subscribers (controllers & services) to be notified when change is made.
-     *
-     * @param {function} callback Callback to execute after a change is made.
-     * @param {boolean} [unsubscribeOnStateChange = true]
-     * @returns {function} Function used to unsubscribe.
-     */
-    subscribeToChanges(callback: Function, unsubscribeOnStateChange = true): Function {
-        const syncPoint = this;
-        if (syncPoint.subscriptions.indexOf(callback) === -1) {
-            /** Only register new subscriptions, ignore if subscription already exists */
-            syncPoint.subscriptions.push(callback);
-        }
-
-        const unsubscribe = () => this.unsubscribe(callback);
-
-        if (unsubscribeOnStateChange) {
-            /** Unsubscribe from notifications when we leave this state */
-            $rootScope.$on('$stateChangeStart', () => {
-                unsubscribe();
-            });
-        }
-
-        return unsubscribe;
-    }
-
-    unsubscribe(callback) {
-        const index = this.subscriptions.indexOf(callback);
-        if (index !== -1) {
-            this.subscriptions.splice(index, 1);
-        }
-    }
+  }
 }
--- src/sync.service.ts
+++ src/sync.service.ts
@@ -1,42 +1,41 @@
 import { Model } from 'angular-point';
-
 import { Lock } from './lock.factory';
 import { ISyncPoint, ISyncServiceInitializationParams, SyncPoint } from './sync-point.factory';
 
 export let $q,
-    $firebaseArray,
-    $rootScope,
-    apListItemFactory,
-    deferred: ng.IDeferred<ISyncServiceInitializationParams>,
-    serviceIsInitialized: ng.IPromise<ISyncServiceInitializationParams>;
+  $firebaseArray,
+  $rootScope,
+  apListItemFactory,
+  deferred: ng.IDeferred<ISyncServiceInitializationParams>,
+  serviceIsInitialized: ng.IPromise<ISyncServiceInitializationParams>;
 
 export class SyncService {
-    /** Minification safe - we're using leading and trailing underscores but gulp plugin doesn't treat them correctly */
-    static $inject = ['$firebaseArray', '$q', 'apListItemFactory', '$rootScope'];
-    Lock = Lock;
-    constructor(_$firebaseArray_, _$q_, _apListItemFactory_, _$rootScope_) {
-        /** Expose to service scope */
-        $q = _$q_;
-        $firebaseArray = _$firebaseArray_;
-        apListItemFactory = _apListItemFactory_;
-        $rootScope = _$rootScope_;
+  /** Minification safe - we're using leading and trailing underscores but gulp plugin doesn't treat them correctly */
+  static $inject = ['$firebaseArray', '$q', 'apListItemFactory', '$rootScope'];
+  Lock = Lock;
+  constructor(_$firebaseArray_, _$q_, _apListItemFactory_, _$rootScope_) {
+    /** Expose to service scope */
+    $q = _$q_;
+    $firebaseArray = _$firebaseArray_;
+    apListItemFactory = _apListItemFactory_;
+    $rootScope = _$rootScope_;
 
-        /** Create a deferred object that will allow service to proceed once a userId is provided */
-        deferred = $q.defer();
-        serviceIsInitialized = deferred.promise;
-    }
+    /** Create a deferred object that will allow service to proceed once a userId is provided */
+    deferred = $q.defer();
+    serviceIsInitialized = deferred.promise;
+  }
 
-    createSyncPoint(model: Model): ISyncPoint {
-        return new SyncPoint(model);
-    }
+  createSyncPoint(model: Model): ISyncPoint {
+    return new SyncPoint(model);
+  }
 
-    /**
-     * @description Service waits for userId to be provided along with reference to the databse before adding the watch to event array.
-     * @param {number} userId
-     * @param {firebase.database.Reference} firebaseRef
-     */
-    initialize(userId: number, firebaseRef: firebase.database.Reference) {
-        deferred.resolve({ userId, firebaseRef });
-        apListItemFactory.ListItem.prototype.lock = Lock;
-    }
+  /**
+   * @description Service waits for userId to be provided along with reference to the databse before adding the watch to event array.
+   * @param {number} userId
+   * @param {firebase.database.Reference} firebaseRef
+   */
+  initialize(userId: number, firebaseRef: firebase.database.Reference) {
+    deferred.resolve({ userId, firebaseRef });
+    apListItemFactory.ListItem.prototype.lock = Lock;
+  }
 }
